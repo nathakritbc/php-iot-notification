071c0eba6d9e4b7dbf4fb546f36854ffd7cb12e0
nathakritbc602 <nathakritbc602@gmail.com>
2023-10-12 16:54:58 +0700

    update

--- functions/telegram-send-ms-image.php
+++ functions/telegram-send-ms-image.php
@@ -1,14 +1,17 @@
 <?php
 $token = '6490529602:AAE-zLE1-WuKg00k0mXlAl01ZC3_TPjEJ14';
-$chatId = '1322124126'; // The chat ID of the user or group you want to send a message to.
-$message = 'สวัสดีครับ Hello, this is a message from your bot!';
+$chatId = '1322124126';  
+ 
 
-$telegramUrl = "https://api.telegram.org/bot$token/sendMessage";
+$stickerPath = '../download.jpg';
+ 
+
+$telegramUrl = "https://api.telegram.org/bot$token/sendSticker";
+
 $data = [
     'chat_id' => $chatId,
-    'text' => $message,
+    'sticker' => new CURLFile(realpath($stickerPath)),
 ];
- 
 
 $ch = curl_init($telegramUrl);
 curl_setopt($ch, CURLOPT_POST, 1);
@@ -19,8 +22,8 @@ $response = curl_exec($ch);
 curl_close($ch);
 
 if ($response === false) {
-    echo 'Error: ' . curl_error($ch);
+    echo 'Error sending the sticker.';
 } else {
-    echo 'Message sent!';
+    echo 'Sticker sent successfully.';
 }
 ?>
