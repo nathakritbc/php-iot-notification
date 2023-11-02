d521a97d7a78599221d3d4c78419ed678bcc33b8
nathakritbc602 <nathakritbc602@gmail.com>
2023-11-02 11:06:11 +0700

    ad

--- insert-usp-data.php
+++ insert-usp-data.php
@@ -14,7 +14,7 @@
 
 <body>
 
-    <button type="button" class="btn btn-outline-primary">Primary</button>
+    <button type="button" class="btn btn-primary">Primary</button>
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.9.2/umd/popper.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.1.0/js/bootstrap.min.js"></script>
