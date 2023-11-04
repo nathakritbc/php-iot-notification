1ba1d001e216aea49b9855de3b0ab845f02ff535
nathakritbc602 <nathakritbc602@gmail.com>
2023-11-04 13:55:10 +0700

    a

--- reboot2.php
+++ reboot2.php
@@ -1,5 +1,14 @@
 <?php
-shell_exec("/usr/sbin/reboot");
-exec("/usr/sbin/reboot");
-system("/usr/sbin/reboot");
+$interface = 'eth0';
+$ipAddress = '192.168.1.101';
+$netmask = '255.255.255.0';
+
+// คำสั่ง ifconfig เพื่อกำหนดที่อยู่ IP แบบคงที่
+$command = "sudo ifconfig $interface $ipAddress netmask $netmask";
+
+// ใช้ shell_exec() เรียกคำสั่ง
+$result = shell_exec($command);
+
+// แสดงผลลัพธ์
+echo "<pre>$result</pre>";
 ?>
