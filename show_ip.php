285d461e178d8e7075660c4e1082c741fa0f7c97
nathakritbc602 <nathakritbc602@gmail.com>
2023-11-04 17:12:20 +0700

    a

--- show_ip.php
+++ show_ip.php
@@ -1,12 +1,12 @@
 <?php
-// รันคำสั่ง ifconfig เพื่อดึงข้อมูลเครือข่าย
-$ifconfigOutput = shell_exec('ifconfig');
+// รันคำสั่ง ifconfig wlan0 เพื่อดึงข้อมูลเครือข่ายสำหรับอินเตอร์เฟส wlan0
+$ifconfigOutput = shell_exec('ifconfig wlan0');
 
 // ใช้ preg_match เพื่อค้นหาเลข IP address จากผลลัพธ์ของ ifconfig
 if (preg_match('/inet (\d+\.\d+\.\d+\.\d+)/', $ifconfigOutput, $matches)) {
     $ipAddress = $matches[1];
-    echo "Your IP Address is: $ipAddress";
+    echo "Your IP Address for wlan0 is: $ipAddress";
 } else {
-    echo "Unable to retrieve IP Address.";
+    echo "Unable to retrieve IP Address for wlan0.";
 }
 ?>
