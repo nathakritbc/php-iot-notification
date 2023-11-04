41d016321905c91433dcc32255a113d2d8bc3deb
nathakritbc602 <nathakritbc602@gmail.com>
2023-11-04 17:03:42 +0700

    ฟ

--- show_ip.php
+++ show_ip.php
@@ -1,23 +1,7 @@
 <?php
-// ใช้ shell_exec() เรียกคำสั่ง ifconfig เพื่อดึงข้อมูลเครือข่าย
-$ifconfigOutput = shell_exec('ifconfig');
+// ดึงเลข IP address จาก PHP
+$ipAddress = $_SERVER['REMOTE_ADDR'];
 
-// แยกข้อมูลเครือข่ายออกจากผลลัพธ์ของ ifconfig
-$ipAddress = '';
-$subnetMask = '';
-$gateway = '';
-
-echo $ifconfigOutput;
-
-// ใช้ preg_match เพื่อค้นหาข้อมูลที่คุณต้องการ
-if (preg_match('/inet (\d+\.\d+\.\d+\.\d+).+netmask (\d+\.\d+\.\d+\.\d+).+gateway (\d+\.\d+\.\d+\.\d+)/s', $ifconfigOutput, $matches)) {
-    $ipAddress = $matches[1];
-    $subnetMask = $matches[2];
-    $gateway = $matches[3];
-}
-
-// แสดงข้อมูลที่ดึงมา
-echo "IP Address: $ipAddress<br>";
-echo "Subnet Mask: $subnetMask<br>";
-echo "Gateway: $gateway<br>";
+// แสดงเลข IP address ใน HTML
+echo "Your IP Address is: $ipAddress";
 ?>
