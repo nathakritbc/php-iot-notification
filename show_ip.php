33fb1bfb138d51e012e1b3086349ccb227244fc2
nathakritbc602 <nathakritbc602@gmail.com>
2023-11-04 17:18:09 +0700

    s

--- show_ip.php
+++ show_ip.php
@@ -1,29 +1,38 @@
 <?php
 
 // รันคำสั่ง ifconfig eth0 เพื่อดึงข้อมูลเครือข่ายสำหรับอินเตอร์เฟส eth0
-$ifconfigOutput = shell_exec('ifconfig eth0');
+// $ifconfigOutput = shell_exec('ifconfig eth0');
 
-// ใช้ preg_match เพื่อค้นหาเลข IP address จากผลลัพธ์ของ ifconfig
-if (preg_match('/inet (\d+\.\d+\.\d+\.\d+)/', $ifconfigOutput, $matches)) {
-    $ipAddress = $matches[1];
-    echo "Your IP Address for eth0 is: $ipAddress";
-} else {
-    echo "Unable to retrieve IP Address for eth0.";
-}
+// // ใช้ preg_match เพื่อค้นหาเลข IP address จากผลลัพธ์ของ ifconfig
+// if (preg_match('/inet (\d+\.\d+\.\d+\.\d+)/', $ifconfigOutput, $matches)) {
+//     $ipAddress = $matches[1];
+//     echo "Your IP Address for eth0 is: $ipAddress";
+// } else {
+//     echo "Unable to retrieve IP Address for eth0.";
+// }
 
  
 
 
 // // รันคำสั่ง ifconfig wlan0 เพื่อดึงข้อมูลเครือข่ายสำหรับอินเตอร์เฟส wlan0
-// $ifconfigOutput = shell_exec('ifconfig wlan0');
+$ifconfigOutput = shell_exec('ifconfig wlan0');
+$ipAddress="";
+$netmask="";
 
-// // ใช้ preg_match เพื่อค้นหาเลข IP address จากผลลัพธ์ของ ifconfig
+// ใช้ preg_match เพื่อค้นหาเลข IP address จากผลลัพธ์ของ ifconfig
 // if (preg_match('/inet (\d+\.\d+\.\d+\.\d+)/', $ifconfigOutput, $matches)) {
 //     $ipAddress = $matches[1];
 //     echo "Your IP Address for wlan0 is: $ipAddress";
 // } else {
 //     echo "Unable to retrieve IP Address for wlan0.";
 // }
+
+if (preg_match('/netmask (\d+\.\d+\.\d+\.\d+)/', $ifconfigOutput, $matches)) {
+    $netmask = $matches[1];
+    echo "Your IP Address for wlan0 is: $netmask";
+} else {
+    echo "Unable to retrieve IP Address for wlan0.";
+}
 
  
 
