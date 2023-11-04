93a39198b078d45df31cae0c0dde423598f19408
nathakritbc602 <nathakritbc602@gmail.com>
2023-11-04 11:47:38 +0700

    a

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
     $command = $_POST['command']; // รับคำสั่งจากฟอร์มบนหน้าเว็บ
     // $command="upsc myups@localhost 2>& 1 | grep -v '^Init SSL'";
     // $command="upsc myups@localhost battery.voltage";
-    $output = shell_exec($command); // รันคำสั่งและรับผลลัพธ์
+    $output = exec($command); // รันคำสั่งและรับผลลัพธ์
 
     echo "<pre>$output</pre>";  
   }
